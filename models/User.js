d022949d17082b3605a4a135177d8e14001aba64
julianmolinerisit <julianmolinerisit@gmail.com>
2026-01-21 15:07:09 -0300

    fix: prevent double-hashing of passwords in pre-save hook

--- models/User.js
+++ models/User.js
@@ -15,6 +15,11 @@ UserSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   
   try {
+    // Si la contraseña ya es un hash bcrypt válido, no volver a hashear
+    if (this.password.startsWith('$2a$') || this.password.startsWith('$2b$')) {
+      return next();
+    }
+    
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
