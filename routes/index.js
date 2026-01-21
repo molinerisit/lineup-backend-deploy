d90f7d313587b22c5be9f1790ce47a0b86597ac8
julianmolinerisit <julianmolinerisit@gmail.com>
2026-01-21 14:18:26 -0300

    fix: correct sensor route prefix to /sensors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
 router.use("/auth", require("./auth.routes"));
+router.use("/sensors", require("./sensor.routes"));
 router.use("/", require("./data.routes"));
 router.use("/", require("./device.routes"));
-router.use("/", require("./sensor.routes"));
 router.use("/", require("./webhook.routes"));
 
 module.exports = router;
